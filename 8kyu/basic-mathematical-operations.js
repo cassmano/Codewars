053efb8fcf1d291191244062f38429d18c7f956a
cassmano <cassandramanotham@gmail.com>
2023-10-06 18:44:16 -0400

    8kyu week 6

---
+++ 8kyu/basic-mathematical-operations.js
@@ -0,0 +1,18 @@
+// MY SOLUTION
+
+function basicOp(operation, v1, v2)
+{
+  // Loop if operation is +-*/ do corresponding actoin
+  if( operation == "+") {
+    return v1 + v2
+  }
+  if( operation == "-") {
+    return v1 - v2
+  }
+  if( operation == "*") {
+    return v1 * v2
+  }
+  if( operation == "/") {
+    return v1 / v2
+  }
+}
