d0f09357784e8909525168f1002289d491b84886
cassmano <cassandramanotham@gmail.com>
2023-10-30 19:31:35 -0400

    week 10 8kyu

---
+++ 8kyu/squaring-arg.js
@@ -0,0 +1,10 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+//Parameters- 1 arg
+//Returns an int
+//Psuedocode
+
+//function named square and takes in an arg
+//returns square of it; it = arg
+function square(arg) {
+    return arg * arg
+}
