4caa62941ea9e2889be98b983255dd0e89228113
cassmano <cassandramanotham@gmail.com>
2023-11-23 14:37:42 -0500

    practice

--- 8kyu/squaring-arg.js
+++ 8kyu/squaring-arg.js
@@ -3,8 +3,25 @@
 //Returns an int
 //Psuedocode
 
-//function named square and takes in an arg
-//returns square of it; it = arg
-function square(arg) {
-    return arg * arg
+/*
+Params: 
+1 arg passed in param, 
+data type: number
+Return: square of the arg (note, square is num * by itself )
+Examples 
+function sq(9) = 81 (9*9)
+function sq(4) = 16 (4*4)
+function sq (8) = 64 (8*8)
+ */
+
+//declare a variable that will take in a number that will be squared
+function squared(num){
+    return num * num
 }
+
+
+// //function named square and takes in an arg
+// //returns square of it; it = arg
+// function square(arg) {
+//     return arg * arg
+// }
