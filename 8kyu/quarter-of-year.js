4ebe01e857836a74f815de3b10ea5f9feff56b70
cassmano <cassandramanotham@gmail.com>
2023-11-27 08:11:24 -0500

    practice

--- 8kyu/quarter-of-year.js
+++ 8kyu/quarter-of-year.js
@@ -7,15 +7,37 @@ Constraint:
 1 <= month <= 12 
 */
 
-//My solution
-const quarterOf = (month) => {
-    if (month <= 3) {
-    return 1
-    } else if (month >= 4 && month <= 6) {
-    return 2
-    } else if (month >= 7 && month <= 9) {
-    return 3
-    } else if (month >= 10 && month <= 12) {
-    return 4
-    } 
+/*
+Data type: ints (all nums), month <= 1 and <= 12 (since there are 12 months)
+1,2,3 = q1; 4,5,6 = q2; 7,8,9 = q3; 10,11,12 = q4
+Given a month, 
+*/
+
+function returnQuarter() {
+    if (month >=1 && month <= 3) {
+        return 1
+    } else if (month >=4 && month <= 6) {
+        return 2
+    } else if (month >=7 && month <= 9) {
+        return 3
+    } else if (month >=10 && month <=12) {
+        return 4
+    }
 }
+
+
+
+
+
+// //My solution
+// const quarterOf = (month) => {
+//     if (month <= 3) {
+//     return 1
+//     } else if (month >= 4 && month <= 6) {
+//     return 2
+//     } else if (month >= 7 && month <= 9) {
+//     return 3
+//     } else if (month >= 10 && month <= 12) {
+//     return 4
+//     } 
+// }
