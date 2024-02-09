0b88922114cb3e4805904b797f060ec95cfbd91a
cassmano <cassandramanotham@gmail.com>
2024-02-09 15:50:54 -0500

    what century codesignal practice

--- codesignal/practice.js
+++ codesignal/practice.js
@@ -9,16 +9,28 @@ Example:
 Possible methods: .split() -> [2,0,2,3] .slice()
 */
 
+// function solution(year) {
+//     //take the int and convert it to a string, then use the split method on string to turn into array
+//     let twoDigits = String(year).split('').slice(0,2).join('')
+//     let century = (Number(twoDigits)+1)
+//     return century
+// }
+
+//optimal with math object using ceil function which rounds up by 1 digit 
+
 function solution(year) {
-    //take the int and convert it to a string, then use the split method on string to turn into array
-    let twoDigits = String(year).split('').slice(0,2).join('')
-    let century = (Number(twoDigits)+1)
-    return century
+    if (year <= 100) {
+        return 1; // Return 1 for years within the range of the first century
+    }
+
+    let firstTwoDigits = Math.ceil(year / 100); // Calculate the century based on the first two digits
+    return firstTwoDigits;
 }
+
 
 console.log(solution(1697))
 console.log(solution(1903))
 console.log(solution(2023))
 console.log(solution(2313))
 console.log(solution(1523))
-console.log(solution(101))
+console.log(solution(301))
