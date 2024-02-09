d8fb52e7ba1ab342960eb59bece2ecc241167c4d
cassmano <cassandramanotham@gmail.com>
2024-02-09 15:32:55 -0500

    js objects

--- rithmschool/objects.js
+++ rithmschool/objects.js
@@ -47,3 +47,38 @@ console.log(namesAndHobbies)
 if ("cass" in namesAndHobbies) {
     console.log("cass", namesAndHobbies.cass)
 }
+
+// For each of the exercises below, assume you are starting with the following programming object.
+
+let programming = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isChallenging: true,
+    isRewarding: true,
+    difficulty: 8,
+    jokes: "https://www.devtopics.com/best-programming-jokes/",
+    coffee: 'yes'
+}
+
+// Write the command to add the language “Go” to the end of the languages array.
+let array = programming.languages.push("Go")
+console.log(programming.languages)
+// Change the difficulty to the value of 7.
+programming.difficulty= 7
+console.log(programming.difficulty)
+// Using the delete keyword, write the command to remove the jokes key from the programming object.
+delete programming.jokes
+// Write the command to add a new key called isFun and a value of true to the programming object.
+programming.isFun = true
+console.log(programming)
+// Using a loop, iterate through the languages array and console.log all of the languages.
+for (let language in programming.languages) {
+    console.log(language)
+}
+// Using a loop, console.log all of the keys in the programming object.
+for (let keys in programming) {
+    console.log(keys)
+}
+// Using a loop, console.log all of the values in the programming object.
+for (let key in programming) {
+    console.log(programming[key])
+}
