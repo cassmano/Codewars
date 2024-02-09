0929ede02475342b15fa4a616693bdff8994bdc1
cassmano <cassandramanotham@gmail.com>
2024-02-09 14:47:03 -0500

    basic string data types

---
+++ 8kyu/string-data-types.js
@@ -0,0 +1,28 @@
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+var yes = "yes"
+var no = "no"
+function Dad(){
+  let word = (d1)+(a2)+(d2)
+  return word;
+}
+function Bee(){
+  let word = `${b1}${e2}${e2}`
+  return word;
+}
+function banana(){
+  let word = `${b2}${a2}${n2}${a2}${n2}${a2}`
+  return word;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  return no;
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return no;
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return yes;
+}
